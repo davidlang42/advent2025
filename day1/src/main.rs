abc9e2eba97b5165a6230d915e49b697417b2dee
David Lang <david.lang@displayr.com>
2025-12-01 16:12:02 +1100

    day1 part1

---
+++ day1/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let filename = &args[1];
+        let text = fs::read_to_string(&filename)
+            .expect(&format!("Error reading from {}", filename));
+        let rotations: Vec<isize> = text.lines().map(|s| if s.chars().next().unwrap() == 'R' {
+            s[1..].parse::<isize>().unwrap()
+        } else {
+            -1 * s[1..].parse::<isize>().unwrap()
+        }).collect();
+        let mut dial = 50;
+        let mut count = 0;
+        for r in rotations {
+            dial += r;
+            while dial > 99 {
+                dial -= 100;
+            }
+            while dial < 0 {
+                dial += 100;
+            }
+            if dial == 0 {
+                count += 1;
+            }
+        }
+        println!("Answer: {}", count);
+    } else {
+        println!("Please provide 1 argument: Filename");
+    }
+}
