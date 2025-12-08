db0e6fed5c7847deace398766229c1e692dbda79
David Lang <david.lang@displayr.com>
2025-12-08 16:57:03 +1100

    part1

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::str::FromStr;
 use std::collections::HashSet;
 
-#[derive(Hash, Eq, PartialEq)]
+#[derive(Hash, Eq, PartialEq, Clone, Copy, Debug)]
 struct Pos {
     x: usize,
     y: usize,
@@ -36,19 +36,32 @@ impl Pos {
 }
 
 struct Lights {
-    circuits: Vec<Circuit>
+    junctions: Vec<Pos>,
+    circuits: Vec<HashSet<Pos>>
+}
+
+impl FromStr for Lights {
+    type Err = String;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        let junctions: Vec<Pos> = text.lines().map(|s| s.parse().unwrap()).collect();
+        Ok(Self {
+            junctions,
+            circuits: Vec::new()
+        })
+    }
 }
 
 impl Lights {
-    fn connect_closest_pair(&mut self) {
-        if self.circuits.len() == 1 {
-            panic!("Only 1 circuit");
-        }
+    fn connect_closest_pair(&mut self, more_than_distance: Option<f64>) -> f64 {
         let mut min = None;
-        for i in 0..self.circuits.len() {
-            for j in (i+1)..self.circuits.len() {
-                let d = self.circuits[i].minimum_distance(&self.circuits[j]);
-                if let Some((min_d, min_i, min_j)) = min {
+        for i in 0..self.junctions.len() {
+            for j in (i+1)..self.junctions.len() {
+                let d = self.junctions[i].distance(&self.junctions[j]);
+                if more_than_distance.is_some() && d <= more_than_distance.unwrap() {
+                    continue;
+                }
+                if let Some((min_d, _, _)) = min {
                     if d < min_d {
                         min = Some((d, i, j));
                     }
@@ -57,87 +70,72 @@ impl Lights {
                 }
             }
         }
-        let (_, i, j) = min.unwrap();
-        self.connect_circuits(i, j);
+        let (d, i, j) = min.unwrap();
+        self.connect_junctions(self.junctions[i], self.junctions[j]);
+        d
     }
 
-    fn connect_circuits(&mut self, i: usize, j: usize) {
-        if j < i {
-            panic!("Connect circuits in increasing order")
-        }
-        let removed = self.circuits.remove(j);
-        for junction in removed.junctions.into_iter() {
-            self.circuits[i].junctions.insert(junction);
-        }
-    }
-
-    fn multiply_three_largest_circuits(&self) -> usize {
-        let mut sizes: Vec<usize> = self.circuits.iter().map(|c| c.junctions.len()).collect();
-        sizes.sort();
-        sizes.reverse();
-        sizes[0] * sizes[1] * sizes[2]
-    }
-}
-
-impl FromStr for Lights {
-    type Err = String;
-
-    fn from_str(text: &str) -> Result<Self, Self::Err> {
-        let junctions: Vec<Pos> = text.lines().map(|s| s.parse().unwrap()).collect();
-        let circuits: Vec<Circuit> = junctions.into_iter().map(Circuit::single).collect();
-        Ok(Self {
-            circuits
-        })
-    }
-}
-
-struct Circuit {
-    junctions: HashSet<Pos>
-}
-
-impl Circuit {
-    fn minimum_distance(&self, other: &Circuit) -> f64 {
-        let mut min = None;
-        for i in self.junctions.iter() {
-            for j in other.junctions.iter() {
-                let d = i.distance(j);
-                if let Some(min_d) = min {
-                    if d < min_d {
-                        min = Some(d)
-                    }
-                } else {
-                    min = Some(d);
-                }
+    fn connect_junctions(&mut self, a: Pos, b: Pos) {
+        println!("Connecting junctions: {:?}, {:?}", a, b);
+        if let Some(existing_a) = self.circuits.iter().position(|c| c.contains(&a)) {
+            if let Some(existing_b) = self.circuits.iter().position(|c| c.contains(&b)) {
+                self.combine_circuits(existing_a, existing_b);
+            } else {
+                self.circuits[existing_a].insert(b);
+            }
+        } else {
+            if let Some(existing_b) = self.circuits.iter().position(|c| c.contains(&b)) {
+                self.circuits[existing_b].insert(a);
+            } else {
+                let mut new_circuit = HashSet::new();
+                new_circuit.insert(a);
+                new_circuit.insert(b);
+                self.circuits.push(new_circuit);
             }
         }
-        min.unwrap()
     }
-}
 
-impl Circuit {
-    fn single(junction: Pos) -> Self {
-        let mut junctions = HashSet::new();
-        junctions.insert(junction);
-        Self {
-            junctions
+    fn combine_circuits(&mut self, i: usize, j: usize) {
+        if i == j {
+            return; // already done
         }
+        if i > j {
+            let removed = self.circuits.remove(i);
+            for junction in removed {
+                self.circuits[j].insert(junction);
+            }
+        } else {
+            let removed = self.circuits.remove(j);
+            for junction in removed {
+                self.circuits[i].insert(junction);
+            }
+        }
+    }
+
+    fn circuit_sizes(&self) -> Vec<usize> {
+        let mut sizes: Vec<usize> = self.circuits.iter().map(|c| c.len()).collect();
+        sizes.sort();
+        sizes.reverse();
+        sizes
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
+    if args.len() == 3 {
         let filename = &args[1];
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
+        let connections: usize = args[2].parse().unwrap();
         let mut lights: Lights = text.parse().unwrap();
-        for i in 0..9 {
-            lights.connect_closest_pair();
+        let mut last_distance = None;
+        for _ in 0..connections {
+            last_distance = Some(lights.connect_closest_pair(last_distance));
         }
-        
-
-        println!("Answer: {}", lights.multiply_three_largest_circuits());
+        let sizes = lights.circuit_sizes();
+        println!("Circuits: {:?}", sizes);
+        println!("Result: {}", sizes[0]*sizes[1]*sizes[2]);
     } else {
-        println!("Please provide 1 argument: Filename");
+        println!("Please provide 2 arguments: Filename, Connections");
     }
 }
