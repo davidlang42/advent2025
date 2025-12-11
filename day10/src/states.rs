a11d490dd53a837334dbe6855a0e50c812e6a913
David Lang <david.lang@displayr.com>
2025-12-11 13:18:06 +1100

    slower than recursion

--- day10/src/states.rs
+++ day10/src/states.rs
@@ -96,53 +96,34 @@ impl JoltageState {
     }
 
     pub fn successors(&self, all_buttons: &Vec<Button>, goal: &Self) -> Vec<(Self, u32)> {
-        let (_index, remaining, buttons) = self.find_fewest_available_buttons(all_buttons, goal);
+        let (remaining_presses, available_buttons) = self.find_fewest_available_buttons(all_buttons, goal);
         let mut v = Vec::new();
-        for option in Self::combinations_of_button_presses(self, &buttons, remaining, goal) {
-            v.push((option, remaining as u32));
-        }
-        v
-    }
-
-    fn combinations_of_button_presses(initial_state: &Self, remaining_buttons: &[&Button], remaining_presses: usize, goal: &JoltageState) -> Vec<Self> {
-        let mut v = Vec::new();
-        if remaining_buttons.len() == 1 {
-            let mut new_state = initial_state.clone();
-            remaining_buttons[0].push(&mut new_state, remaining_presses); // only 1 button left, press it the remaining times
-            if new_state.is_valid(goal) {
-                v.push(new_state);
-            }
-        } else {
-            // try the first button any number of times
+        for button in available_buttons {
             for times in 0..(remaining_presses + 1) {
-                let mut new_state = initial_state.clone();
-                remaining_buttons[0].push(&mut new_state, times);
-                if !new_state.is_valid(goal) {
-                    continue;
-                }
-                // then generate combinations for remaining buttons & presses
-                for option in Self::combinations_of_button_presses(&new_state, &remaining_buttons[1..], remaining_presses - times, goal) {
-                    v.push(option);
+                let mut new_state = self.clone();
+                button.push(&mut new_state, times);
+                if new_state.is_valid(goal) {
+                    v.push((new_state, times as u32));
                 }
             }
         }
         v
     }
 
-    fn find_fewest_available_buttons<'a>(&self, buttons: &'a Vec<Button>, goal: &Self) -> (usize, usize, Vec<&'a Button>) {
-        let mut min: Option<(usize, usize, Vec<&Button>)>  = None;
+    fn find_fewest_available_buttons<'a>(&self, buttons: &'a Vec<Button>, goal: &Self) -> (usize, Vec<&'a Button>) {
+        let mut min: Option<(usize, Vec<&Button>)>  = None;
         for i in 0..self.0.len() {
             if self.0[i] == goal.0[i] {
                 continue; // already finished
             }
             let remaining = goal.0[i] - self.0[i];
             let available_buttons: Vec<_> = buttons.iter().filter(|b| b.affects(i)).collect();
-            if let Some((_min_i, _min_r, min_b)) = &min {
+            if let Some((_min_r, min_b)) = &min {
                 if available_buttons.len() < min_b.len() {
-                    min = Some((i, remaining, available_buttons));
+                    min = Some((remaining, available_buttons));
                 }
             } else {
-                min = Some((i, remaining, available_buttons));
+                min = Some((remaining, available_buttons));
             }
         }
         min.unwrap()
